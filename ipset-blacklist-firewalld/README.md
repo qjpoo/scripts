8efebe2a57d096282ce0143305b11def890c342e
瞿健 <qujian@51-dg.com>
2020-12-23 15:19:31 +0800

    add

---
+++ ipset-blacklist-firewalld/README.md
@@ -0,0 +1,66 @@
+ipset-blacklist-firewalld
+===============
+
+A Bash shell script which uses firewall-cmd to ban a large number of IP addresses published in IP blacklists. firewalld ipset uses a hashtable to store/fetch IP addresses and thus the IP lookup is a lot (!) faster than thousands of sequentially parsed iptables ban rules.
+
+This script is borrowed and modified from [ipset-blacklist](https://github.com/trick77/ipset-blacklist) to use firewalld and its built-in ipset implementation.
+
+## Quick start for Debian/Ubuntu based installations
+1. wget -O /usr/local/sbin/update-blacklist.sh https://raw.githubusercontent.com/joe-at-startupmedia/ipset-blacklist-firewalld/master/update-blacklist.sh
+1. chmod +x /usr/local/sbin/update-blacklist.sh
+1. mkdir -p /etc/ipset-blacklist-firewalld ; wget -O /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf https://raw.githubusercontent.com/joe-at-startupmedia/ipset-blacklist-firewalld/master/ipset-blacklist-firewalld.conf
+1. Modify ipset-blacklist-firewalld.conf according to your needs. Per default, the blacklisted IP addresses will be saved to /etc/ipset-blacklist-firewalld/ip-blacklist.restore
+1. Auto-update the blacklist using a cron job
+
+## First run, create the list
+to generate the /etc/ipset-blacklist-firewalld/ip-blacklist.list
+```
+/usr/local/sbin/update-blacklist.sh /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf
+```
+
+## iptables filter rule
+```
+# Enable blacklists
+firewall-cmd --permanent --ipset --ipset=blacklist --add-entries-from-file=/etc/ipset-blacklist-firewalld/ip-blacklist.list
+firewall-cmd --reload
+```
+Make sure to run this snippet in a firewall script or just insert it to /etc/rc.local.
+
+## Cron job
+In order to auto-update the blacklist, copy the following code into /etc/cron.d/update-blacklist. Don't update the list too often or some blacklist providers will ban your IP address. Once a day should be OK though.
+```
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+33 23 * * *      root /usr/local/sbin/update-blacklist.sh /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf
+```
+
+## Check for dropped packets
+Using iptables, you can check how many packets got dropped using the blacklist:
+
+```
+drfalken@wopr:~# iptables -L INPUT -v --line-numbers
+Chain INPUT (policy DROP 60 packets, 17733 bytes)
+num   pkts bytes target            prot opt in  out source   destination
+1       15  1349 DROP              all  --  any any anywhere anywhere     match-set blacklist src
+2        0     0 fail2ban-vsftpd   tcp  --  any any anywhere anywhere     multiport dports ftp,ftp-data,ftps,ftps-data
+3      912 69233 fail2ban-ssh-ddos tcp  --  any any anywhere anywhere     multiport dports ssh
+4      912 69233 fail2ban-ssh      tcp  --  any any anywhere anywhere     multiport dports ssh
+```
+
+## Modify the blacklists you want to use
+Edit the BLACKLIST array in /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf to add or remove blacklists, or use it to add your own blacklists.
+```
+BLACKLISTS=(
+"http://www.mysite.me/files/mycustomblacklist.txt" # Your personal blacklist
+"http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1" # Project Honey Pot Directory of Dictionary Attacker IPs
+# I don't want this: "http://www.openbl.org/lists/base.txt"  # OpenBL.org 30 day List
+)
+```
+If you for some reason want to ban all IP addresses from a certain country, have a look at [IPverse.net's](http://ipverse.net/ipblocks/data/countries/) aggregated IP lists which you can simply add to the BLACKLISTS variable. For a ton of spam and malware related blacklists, check out this github repo: https://github.com/firehol/blocklist-ipsets
+
+## Remove the firewall
+```
+firewall-cmd --permanent --delete-ipset=blacklist
+firewall-cmd --zone=drop --remove-source=ipset:blacklist
+firewall-cmd --reload
+```
---
+++ ipset-blacklist-firewalld/update-blacklist.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+#
+# usage update-blacklist.sh <configuration file>
+# eg: update-blacklist.sh /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf
+#
+function exists() { command -v "$1" >/dev/null 2>&1 ; }
+
+function ipset_flush() {
+  FLUSH_TMP=$(mktemp)
+  firewall-cmd --permanent --ipset="$IPSET_BLACKLIST_NAME" --get-entries > "$FLUSH_TMP" 
+  firewall-cmd --permanent --ipset="$IPSET_BLACKLIST_NAME" --remove-entries-from-file="$FLUSH_TMP"
+  rm -f "$FLUSH_TMP"
+}
+
+if [[ -z "$1" ]]; then
+  echo "Error: please specify a configuration file, e.g. $0 /etc/ipset-blacklist-firewalld/ipset-blacklist-firewalld.conf"
+  exit 1
+fi
+
+if ! source "$1"; then
+  echo "Error: can't load configuration file $1"
+  exit 1
+fi
+
+if ! exists curl && exists egrep && exists grep && exists firewall-cmd && exists sed && exists sort && exists wc ; then
+  echo >&2 "Error: searching PATH fails to find executables among: curl egrep grep firewall-cmd sed sort wc"
+  exit 1
+fi
+
+DO_OPTIMIZE_CIDR=no
+if exists iprange && [[ ${OPTIMIZE_CIDR:-yes} != no ]]; then
+  DO_OPTIMIZE_CIDR=yes
+fi
+
+if [[ ! -d $(dirname "$IP_BLACKLIST") ]]; then
+  echo >&2 "Error: missing directory(s): $(dirname "$IP_BLACKLIST" |sort -u)"
+  exit 1
+fi
+
+# create the ipset if needed (or abort if does not exists and FORCE=no)
+if ! firewall-cmd --permanent --get-ipsets|command grep -q "$IPSET_BLACKLIST_NAME"; then
+  if [[ ${FORCE:-no} != yes ]]; then
+    echo >&2 "Error: ipset does not exist yet, add it using:"
+    echo >&2 "# firewall-cmd --permanent --new-ipset=\"$IPSET_BLACKLIST_NAME\" --type=hash:net --option=family=inet --option=hashsize=\"${HASHSIZE:-16384}\" --option=maxelem=\"${MAXELEM:-65536}\""
+    exit 1
+  fi
+  if ! firewall-cmd --permanent --new-ipset="$IPSET_BLACKLIST_NAME" --type=hash:net --option=family=inet --option=hashsize="${HASHSIZE:-16384}" --option=maxelem="${MAXELEM:-65536}"; then
+    echo >&2 "Error: while creating the initial ipset"
+    exit 1
+  fi
+fi
+
+# parameter substitution for the dropzone
+DEFAULT_ZONE="--zone=$(firewall-cmd --get-default-zone)"
+IPSET_DROP_ZONE=${IPSET_DROP_ZONE-$DEFAULT_ZONE}
+
+# add our ipset to drop zone sources (or abort if does not exists and FORCE=no)
+if ! firewall-cmd --permanent "$IPSET_DROP_ZONE" --list-sources|command grep -q "ipset:$IPSET_BLACKLIST_NAME"; then
+  # we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
+  if [[ ${FORCE:-no} != yes ]]; then
+    echo >&2 "Error: firewalld does not have the ipset added to the drop zone, add it using:"
+    echo >&2 "# firewall-cmd --permanent $IPSET_DROP_ZONE --add-source=ipset:$IPSET_BLACKLIST_NAME"
+    exit 1
+  fi
+  if ! firewall-cmd --permanent "$IPSET_DROP_ZONE" --add-source=ipset:"$IPSET_BLACKLIST_NAME"; then
+    echo >&2 "Error: while adding ipset to the drop zone"
+    exit 1
+  fi
+fi
+
+# create a rich rule for the ipset drop zone source (default if none specified)
+if [ "$IPSET_DROP_ZONE" != "--zone=drop" ] && ! firewall-cmd --permanent "$IPSET_DROP_ZONE" --list-rich-rules|command grep -q "ipset=$IPSET_BLACKLIST_NAME"; then
+  if [[ ${FORCE:-no} != yes ]]; then
+    echo >&2 "Error: firewalld does not have the source added to the drop zone, add it using:"
+    echo >&2 "# firewall-cmd --permanent $IPSET_DROP_ZONE --add-rich-rule='rule source ipset=$IPSET_BLACKLIST_NAME drop'"
+    exit 1
+  fi
+  if ! firewall-cmd --permanent "$IPSET_DROP_ZONE" --add-rich-rule="rule source ipset=$IPSET_BLACKLIST_NAME drop"; then
+    echo >&2 "Error: while adding ipset source to the drop zone"
+    exit 1
+  fi
+fi
+
+IP_BLACKLIST_TMP=$(mktemp)
+for i in "${BLACKLISTS[@]}"
+do
+  IP_TMP=$(mktemp)
+  (( HTTP_RC=$(curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP" -s -w "%{http_code}" "$i") ))
+  if (( HTTP_RC == 200 || HTTP_RC == 302 || HTTP_RC == 0 )); then # "0" because file:/// returns 000
+    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_BLACKLIST_TMP"
+    [[ ${VERBOSE:-yes} == yes ]] && echo -n "."
+  elif (( HTTP_RC == 503 )); then
+    echo -e "\\nUnavailable (${HTTP_RC}): $i"
+  else
+    echo >&2 -e "\\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
+  fi
+  rm -f "$IP_TMP"
+done
+
+# sort -nu does not work as expected
+sed -r -e '/^(0\.0\.0\.0|10\.|127\.|172\.1[6-9]\.|172\.2[0-9]\.|172\.3[0-1]\.|192\.168\.|22[4-9]\.|23[0-9]\.)/d' "$IP_BLACKLIST_TMP"|sort -n|sort -mu >| "$IP_BLACKLIST"
+if [[ ${DO_OPTIMIZE_CIDR} == yes ]]; then
+  if [[ ${VERBOSE:-no} == yes ]]; then
+    echo -e "\\nAddresses before CIDR optimization: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
+  fi
+  < "$IP_BLACKLIST" iprange --optimize - > "$IP_BLACKLIST_TMP" 2>/dev/null
+  if [[ ${VERBOSE:-no} == yes ]]; then
+    echo "Addresses after CIDR optimization:  $(wc -l "$IP_BLACKLIST_TMP" | cut -d' ' -f1)"
+  fi
+  cp "$IP_BLACKLIST_TMP" "$IP_BLACKLIST"
+fi
+
+rm -f "$IP_BLACKLIST_TMP"
+
+ipset_flush
+firewall-cmd --permanent --ipset="$IPSET_BLACKLIST_NAME" --add-entries-from-file="$IP_BLACKLIST"
+firewall-cmd --reload
+
+if [[ ${VERBOSE:-no} == yes ]]; then
+  echo
+  echo "Blacklisted addresses found: $(wc -l "$IP_BLACKLIST" | cut -d' ' -f1)"
+fi
